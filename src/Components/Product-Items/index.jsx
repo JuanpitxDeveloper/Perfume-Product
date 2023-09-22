ca173dcb6a9809efe443420ffa7d423efa1642ee
JuanpitxDeveloper <juanchojm0161@gmail.com>
2023-09-22 19:35:10 -0400

    Last change

--- src/Components/Product-Items/index.jsx
+++ src/Components/Product-Items/index.jsx
@@ -1,5 +1,6 @@
 import Product from "/image-product-mobile.png" 
 import ProductDesktop from "/image-product-desktop.jpg"
+import iconCart from "/icon-cart.svg"
 export default function ProductDetails() {
     return (
     <main className="w-screen h-[100vh] grid place-content-center  bg-[#f2ebe3] px-[16px] md:flex md:flex-row md:justify-center md:items-center" >
@@ -33,7 +34,7 @@ export default function ProductDetails() {
                 </strike>
                 </h3>
             </div>
-            <button className=" bg-[hsl(158,36%,37%)] rounded-lg text-white py-3.5 mb-6 mt-2 flex justify-center md:mt-3 "><img src=".//public/icon-cart.svg" alt="icon-cart" className=" mr-3" /><span>
+            <button className=" bg-[hsl(158,36%,37%)] rounded-lg text-white py-3.5 mb-6 mt-2 flex justify-center md:mt-3 "><img src={iconCart} alt="icon-cart" className=" mr-3" /><span>
                 Add to Cart</span> 
                 
             </button>
